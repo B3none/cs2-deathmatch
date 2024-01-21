b925fd580404463dadaf26c1579fbd2fbe6be1c7
B3none <ablackham2000@gmail.com>
2024-01-21 03:47:41 +0000

    Added initial Helpers.cs

---
+++ Modules/Helpers.cs
@@ -0,0 +1,11 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace DeathmatchPlugin.Modules;
+
+public static class Helpers
+{
+    public static bool IsValidPlayer(CCSPlayerController? player)
+    {
+        return player != null && player.IsValid;
+    }
+}
