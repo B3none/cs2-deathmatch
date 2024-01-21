96015f91b67cc3bd631aa7a01f8ef22a425b6b49
B3none <ablackham2000@gmail.com>
2024-01-21 04:14:41 +0000

    Silence team join notifications

--- DeathmatchPlugin.cs
+++ DeathmatchPlugin.cs
@@ -1,5 +1,6 @@
 ﻿using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
 
 namespace DeathmatchPlugin;
 
@@ -9,10 +10,19 @@ public class DeathmatchPlugin : BasePlugin
     public override string ModuleName => "Deathmatch Plugin";
     public override string ModuleVersion => "0.0.1";
     public override string ModuleAuthor => "B3none";
-    public override string ModuleDescription => "Free for all deathmatch gamemode for CS2.";
+    public override string ModuleDescription => "Free for all deathmatch game mode for CS2.";
 
     public override void Load(bool hotReload)
     {
         Console.WriteLine("Deathmatch loaded!");
+    }
+
+    [GameEventHandler(HookMode.Pre)]
+    public HookResult OnPlayerTeam(EventPlayerTeam @event, GameEventInfo info)
+    {
+        // Ensure all team join events are silent.
+        @event.Silent = true;
+        
+        return HookResult.Continue;
     }
 }
