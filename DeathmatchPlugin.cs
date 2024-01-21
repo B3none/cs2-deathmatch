1a808c1a3427f7d673d941972cbe75662f7364ec
B3none <ablackham2000@gmail.com>
2024-01-21 03:18:43 +0000

    Added initial DeathmatchPlugin.cs

---
+++ DeathmatchPlugin.cs
@@ -0,0 +1,24 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Cvars;
+using CounterStrikeSharp.API.Modules.Entities.Constants;
+using CounterStrikeSharp.API.Modules.Utils;
+using Microsoft.Extensions.Logging;
+
+namespace DeathmatchPlugin;
+
+[MinimumApiVersion(147)]
+public class DeathmatchPlugin : BasePlugin
+{
+    public override string ModuleName => "Deathmatch Plugin";
+    public override string ModuleVersion => "0.0.1";
+    public override string ModuleAuthor => "B3none";
+    public override string ModuleDescription => "Free for all deathmatch gamemode for CS2.";
+
+    public override void Load(bool hotReload)
+    {
+        Console.WriteLine("Deathmatch loaded!");
+    }
+}
